1c2ee65b33eaa073c98a1221a0d9e90b1563fd97
Aditya <aditya.learning007@gmail.com>
2023-12-04 17:42:36 +0530

    CRUD operation

---
+++ MVC_Project/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public readonly CompanydbContext _context;
+
+        public EmployeeController(CompanydbContext context)
+        { 
+            _context = context;
+        }
+        [HttpGet] //HTTP VERBS
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Save(Employee Obj)
+        {
+            _context.tblEmployee.Add(Obj);
+            _context.SaveChanges();            
+        }
+        [HttpGet]
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ViewEmployee()
+        {
+            Employee obj=new Employee();
+            obj.Address = "Mumbai";
+            return View(obj);
+        }
+        
+        public void UpdateEmployee()
+        {
+            var result = _context.tblEmployee.FirstOrDefault(e => e.Emp_Id == 2);
+            if (result != null)
+            {
+                Employee obj = new Employee();
+                result.Department = "ADMIN";
+                result.LastName = "Pimple";
+                _context.SaveChanges();
+            }
+        }
+        public void getAllEmployee()
+        {
+            List<Employee> lstEmp = _context.tblEmployee.ToList();
+        }
+
+        public void deleteEmployee()
+        {
+            var result = _context.tblEmployee.FirstOrDefault(e => e.Emp_Id == 2);
+            if(result != null)
+            {
+                _context.tblEmployee.Remove(result);
+                _context.SaveChanges();
+            }
+
+        }
+
+    }
+}
--- MVC_Project/Program.cs
+++ MVC_Project/Program.cs
@@ -38,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Test}/{id?}");
+    pattern: "{controller=Employee}/{action=Index}/{id?}");
 
 app.Run();
